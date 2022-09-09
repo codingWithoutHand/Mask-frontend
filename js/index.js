f4bff4456de7ace9a2cdc5674a336bc85823d68e
EXON <exon.indiv@gmail.com>
2022-09-09 09:27:39 +0000

    Update: white theme

--- js/index.js
+++ js/index.js
@@ -1,6 +1,28 @@
-const registerHomeBtn = document.getElementById('registerHomeBtn')
+class App {
+    constructor() {
+        const registerHomeBtn = document.getElementById('registerHomeBtn')
 
-// 집 등록하기
-registerHomeBtn.onclick = () => {
-    alert('현재 위치가 집으로 등록되었습니다.')
+        if (this.isDarkMode()) {
+            const bg = document.body.style.getPropertyValue('--bg')
+            alert(bg)
+        } else {
+            const bg = document.body.style.getPropertyValue('--bg')
+            alert(bg)
+        }
+        
+        // 집 등록하기
+        registerHomeBtn.onclick = () => {
+            alert('현재 위치가 집으로 등록되었습니다.')
+        }
+    }
+    
+
+
+
+    isDarkMode() {
+        window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+    }
 }
+
+window.onload(() => new App())
